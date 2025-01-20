6f14ebe904549a865a7d3907b02c6ea17dcac037
chaitanya-sai-neeli <chaitanyasaichaitu123@gmail.com>
2025-01-20 23:47:56 +0530

    Formatted changes

--- src/components/UserClass2.js
+++ src/components/UserClass2.js
@@ -1,40 +1,43 @@
 import React from "react";
 class UserClass2 extends React.Component {
+  constructor(props) {
+    super(props);
 
-    constructor(props){
-        super(props);
+    this.state = {
+      count: 0,
+    };
+    console.log(this.props.name + " Child Constructor");
+  }
 
-        this.state = {
-            count : 0
-        }   
-        console.log(this.props.name+ " Child Constructor");
-    }
+  componentDidMount() {
+    console.log(this.props.name + " Child comp Mounted");
+  }
 
-    componentDidMount(){
-        console.log(this.props.name+" Child comp Mounted");
-    }
-
-    render(){
-        const {name, place} = this.props
-        console.log(this.props.name+  " Child render");
-        return (
-            <div className = "user-card">
-                <h2>{name}</h2>
-                <h3>{place}</h3>
-                <h3>Count: {this.state.count}</h3>
-                <button onClick = {() => {
-                    this.setState(
-                        // (prevState)=>({count : prevState.count + 1})   //Arrow function syntax
-                        // function(prevState){
-                        //     return {count: prevState.count + 1};      //Normal function syntax returning the object not just value or expression
-                        // }
-                        {count: this.state.count + 1}
-                    )
-                }}>Increment</button>
-                <h4>Phone: 7780558397</h4>
-            </div>
-        )
-    }
+  render() {
+    const { name, place } = this.props;
+    console.log(this.props.name + " Child render");
+    return (
+      <div className="user-card">
+        <h2>{name}</h2>
+        <h3>{place}</h3>
+        <h3>Count: {this.state.count}</h3>
+        <button
+          onClick={() => {
+            this.setState(
+              // (prevState)=>({count : prevState.count + 1})   //Arrow function syntax
+              // function(prevState){
+              //     return {count: prevState.count + 1};      //Normal function syntax returning the object not just value or expression
+              // }
+              { count: this.state.count + 1 }
+            );
+          }}
+        >
+          Increment
+        </button>
+        <h4>Phone: 1234567890</h4>
+      </div>
+    );
+  }
 }
 
 export default UserClass2;
